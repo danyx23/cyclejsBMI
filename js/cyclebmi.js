fca239690036a933b6996ea5809f112444a3c872
Daniel Bachler <daniel@danielbachler.de>
2015-04-09 02:04:07 +0200

    Implemented base version targeting new simplified stream version of cycle js. Nicer and cleaner. Current version has a little bug in that the first slider is displayed at the wrong value initially.

--- js/cyclebmi.js
+++ js/cyclebmi.js
@@ -1,5 +1,4 @@
 /** @jsx dom */
-const Cycle = require("../../cycle/dist/cycle.js");
 const {Rx} = Cycle;
 const h = Cycle.h;
 
@@ -13,25 +12,30 @@ Cycle.registerCustomElement("slider", (rootElement$, props) => {
 	const model = function() {
 		const value$ = Cycle.createStream((changeValue$, propsStartValue$) => {
 						return propsStartValue$
-								.startWith(0)
-								.merge(changeValue$);
-
+							.merge(changeValue$);
+		});
+		const min$ = Cycle.createStream((propsMin$) => {
+			return propsMin$.shareReplay(1);
+		});
+		const max$ = Cycle.createStream((propsMax$) => {
+			return propsMax$.shareReplay(1);
 		});
 		return {
 			value$,
-			inject: function inject(props, intent) {
-				value$.inject(intent.changeValue$, props.get('foo$'));
+			min$,
+			max$,
+			inject(props, intent) {
+				value$.inject(intent.changeValue$, props.get('value$'));
+				min$.inject(props.get('min$'));
+				max$.inject(props.get('max$'));
 				return [props, intent];
-		};
+		}};
 	}();
 
 	const view = function() {
-		Cycle.createView(Model => {
-			const value$ = Model.get("value$");
-			const min$ = Props.get("min$");
-			const max$ = Props.get("max$");
-			return {
-				vtree$: Rx.Observable.combineLatest(
+		const vtree$ = Cycle.createStream((value$, min$, max$) => {
+
+			return Rx.Observable.combineLatest(
 					value$,
 					min$,
 					max$,
@@ -45,75 +49,138 @@ Cycle.registerCustomElement("slider", (rootElement$, props) => {
 								</div>
 							</div>
 						</div>
-					))
-			};
+					));
 		});
-	}
+		return {
+			vtree$,
+			inject(model) {
+				vtree$.inject(model.value$, model.min$, model.max$);
+				return model;
+			}
+		};
+	}();
 
 	function parseValue(val) {
-		const parsed = parseInt(event.target.value, 10);
+		const parsed = parseInt(val, 10);
 		return parsed;
 	}
 
-	const Intent = Cycle.createIntent(User => {
-		const changeSlider$ = User.event$("[type=range]", "input")
-			.map(event => parseValue(event.target.value));
-
-		const changeInput$ = User.event$("[type=text]", "input")
-			.map(event => parseValue(event.target.value))
-			.filter(val => !Number.isNaN(val));
+	const user = (function () {
 		return {
-			changeSlider$,
-			changeInput$,
-			changeValue$: Rx.Observable.merge(changeSlider$, changeInput$)
+			interactions$: rootElement$.interactions$,
+			inject(view) {
+				rootElement$.inject(view.vtree$);
+				return view;
+			}
 		};
-	});
-	User.inject(View).inject(Model).inject(Intent, Props)[0].inject(User);
+	})();
+
+	const intent = (function () {
+			const changeSlider$ = Cycle.createStream(interactions$ =>
+				interactions$.choose("[type=range]", "input")
+				.map(event => parseValue(event.target.value)));
+
+			const changeInput$ = Cycle.createStream(interactions$ =>
+				interactions$.choose("[type=text]", "input")
+				.map(event => parseValue(event.target.value))
+				.filter(val => !Number.isNaN(val)));
+			return {
+				changeSlider$,
+				changeInput$,
+				changeValue$: Rx.Observable.merge(changeSlider$, changeInput$),
+				inject(user) {
+					changeSlider$.inject(user.interactions$);
+					changeInput$.inject(user.interactions$);
+					return user;
+				}
+			};
+	})();
+
+	user.inject(view).inject(model).inject(props, intent)[1].inject(user);
+
 	return {
-		changeValue$: Intent.get("changeValue$").tap(x => console.log("slider changed to: " + x))
+		changeValue$: intent.changeValue$.tap(x => console.log("slider changed to: " + x))
 	};
 });
 
-const Model = Cycle.createModel(Intent => {
+const model = (function () {
+	const height$ = Cycle.createStream(changeHeight$ => {
+		return changeHeight$.startWith(175);
+	});
+	const mass$ = Cycle.createStream(changeMass$ => {
+		return changeMass$.startWith(75);
+	});
 	return {
-		height$: Intent.get("changeHeight$").startWith(175),
-		mass$: Intent.get("changeMass$").startWith(75)
-	};
-});
+		height$,
+		mass$,
+		inject(intent) {
+			height$.inject(intent.changeHeight$);
+			mass$.inject(intent.changeMass$);
+			return intent;
+		}
+	}
+})();
 
 const calculateBMI = (height, mass) =>	{
 	console.log('bmi', height, mass);
  	return Math.round(mass / Math.pow(height / 100, 2));
 };
 
-const View = Cycle.createView(Model => {
-	return {
-		vtree$: Rx.Observable.combineLatest(
-		Model.get("height$"),
-		Model.get("mass$"),
+const view = (function () {
+	const vtree$ = Cycle.createStream((height$, mass$) => {
+		return Rx.Observable.combineLatest(
+		height$,
+		mass$,
 		(height, mass) => (
 			<div class={"everything"}>
 				<div>
-					<slider className="slider-height" value={height} min={130} max={220}/>
-					<slider className="slider-mass" value={mass} min={25} max={150}/>
+					<slider className="slider-height" value={height} min={130} max={220} key={1}/>
+					<slider className="slider-mass" value={mass} min={25} max={150} key={2}/>
 				</div>
 				<div>
 					Your BMI is: {"" + calculateBMI(height, mass)}
 				</div>
 			</div>
 		)
-		)}
-	}
-);
-
-const Intent = Cycle.createIntent(User => {
+		)});
 	return {
-		changeHeight$: User.event$(".slider-height", "changeValue").map(event => event.data),
-		changeMass$: User.event$(".slider-mass", "changeValue").map(event => event.data)
+		vtree$,
+		inject(model) {
+			vtree$.inject(model.height$, model.mass$);
+			return model;
+		}
+	}
+})();
+
+const user = (function () {
+	const interactions$ = Cycle.createStream(function (vtree$) {
+		return Cycle.render(vtree$, '.app').interactions$;
+	});
+	return {
+		interactions$,
+		inject(view) {
+			interactions$.inject(view.vtree$);
+			return view;
+		}
 	};
-});
+})();
 
+const intent = (function() {
+	const changeHeight$ = Cycle.createStream(interactions$ => {
+		return interactions$.choose('.slider-height', 'changeValue').map(event => event.data);
+	});
+	const changeMass$ = Cycle.createStream(interactions$ => {
+		return interactions$.choose('.slider-mass', 'changeValue').map(event => event.data);
+	});
+	return {
+		changeHeight$,
+		changeMass$,
+		inject(user) {
+			changeHeight$.inject(user.interactions$);
+			changeMass$.inject(user.interactions$);
+			return user;
+		}
+	};
+})();
 
-const User = Cycle.createDOMUser(".app");
-
-User.inject(View).inject(Model).inject(Intent).inject(User);
+user.inject(view).inject(model).inject(intent).inject(user);
